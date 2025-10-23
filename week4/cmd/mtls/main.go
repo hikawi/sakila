c9e9a1d297bbda66b66b2e9568a296e657e76f24
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-10-23 23:21:44 +0700

    chore: fix wrong context path for all workflows

--- week4/cmd/mtls/main.go
+++ week4/cmd/mtls/main.go
@@ -33,7 +33,7 @@ func main() {
 	// Load CA cert
 	caCert, err := os.ReadFile("./certs/ca.crt")
 	if err != nil {
-		log.Fatalf("fatal: can't find root CA certificate\n")
+		log.Fatalln("fatal: can't find root CA certificate")
 	}
 
 	caPool := x509.NewCertPool()
